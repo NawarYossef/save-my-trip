f6ba382a8cae944e9bc6c45396392353b8fae897
Nawar Yossef <nawaryossef2@gmail.com>
2017-12-22 23:50:31 -0500

    setup config.js file

---
+++ config.js
@@ -0,0 +1,11 @@
+const PORT = process.env.PORT || 8080;
+const DATABASE_NAME = 'tellmeondate';
+const DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || `mongodb://localhost/${DATABASE_NAME}`;
+const TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || `mongodb://localhost/test-${DATABASE_NAME}`
+
+module.exports = {
+  PORT,
+  DATABASE_NAME,
+  DATABASE_URL,
+  TEST_DATABASE_URL
+}
