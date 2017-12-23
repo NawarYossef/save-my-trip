600c80a16fd03870a420144fc5f78fdc9235e7e7
Nawar Yossef <nawaryossef2@gmail.com>
2017-12-23 01:09:32 -0500

    add basic GET request to root

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const {PORT} = require("./config");
 
 app.use(express.static('public'));
 
+app.get("/", (req, res) => {
+  res.status(200).send("Server is running");
+})
+
 app.listen(port = PORT, () => {
   console.log(`Your app is listening on port ${port}`);
 })
