3b5de5c310ab6f8d8748221d8b66df8f16bef8ed
Nawar Yossef <nawaryossef2@gmail.com>
2017-12-24 16:22:57 -0500

    move requests from server file to router

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const morgan = require("morgan")
 
 mongoose.Promise = global.Promise;
 
-
+const tripsRouter = require('./tripsRouter');
 const {PORT, DATABASE_URL} = require("./config");
 const {Trip} = require("./models")
 
@@ -15,23 +15,57 @@ app.use(bodyParser.json());
 app.use(morgan("common"));
 app.use(express.static('public'));
 
-app.get("/", (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, 'public', 'landing-page.html'));
-})
+app.use('/trips', tripsRouter);
 
-app.get("/dashboard", (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, 'public', 'dashboard.html'));
-})
-
-app.get("/new-trip", (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, 'public', 'new-trip.html'));
-})
+app.use('*', function(req, res) {
+  res.status(404).json({message: 'Not Found'});
+});
 
 
+// closeServer needs access to a server object, but that only
+// gets created when `runServer` runs, so we declare `server` here
+// and then assign a value to it in run
+let server;
 
+// this function connects to our database, then starts the server
+function runServer(databaseUrl = DATABASE_URL, port = PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, {useMongoClient: true}, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
+  });
+}
 
-app.listen(PORT, () => {
-  console.log(`Your app is listening on port ${PORT}`);
-})
+// this function closes the server, and returns a promise. we'll
+// use it in our integration tests later.
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
 
-module.exports = {app};
+// if server.js is called directly (aka, with `node server.js`), this block
+// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer };
--- tripsRouter.js
+++ tripsRouter.js
@@ -6,6 +6,37 @@ const jsonParser = bodyParser.json();
 
 const {Trip} = require('./models');
 
-router.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/dreams.html');
+router.get("/", (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, 'public', 'landing-page.html'));
+})
+
+router.get("/dashboard", (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, 'public', 'dashboard.html'));
+})
+
+router.get("/new-trip", (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, 'public', 'new-trip.html'));
+})
+
+router.get('/trips', (req, res) => {
+  Trip
+    .find()
+    // we're limiting because restaurants db has > 25,000
+    // documents, and that's too much to process/return
+    .limit(10)
+    // success callback: for each restaurant we got back, we'll
+    // call the `.serialize` instance method we've created in
+    // models.js in order to only expose the data we want the API return.
+    .then(trips => {
+      res.json({
+        trips: trips.map(
+          (trip) => trip.serialize())
+      });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error' });
+    });
 });
+
+module.exports = router;
