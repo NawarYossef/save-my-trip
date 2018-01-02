3664efde9b36a034ad86e1827d88005d8cb9f937
Nawar Yossef <nawaryossef2@gmail.com>
2018-01-02 14:08:31 -0500

    use coorect route for GET test requests

--- test/test-trips.js
+++ test/test-trips.js
@@ -1,18 +1,14 @@
 const chai = require("chai");
 const chaiHttp = require('chai-http');
-const {app, runServer, closeServer} = require("../server")
-const {TEST_DATABASE_URL} = require("../config")
-const {Trip} = require("../models")
+const { app, runServer, closeServer } = require("../server")
+const { TEST_DATABASE_URL } = require("../config")
+const { Trip } = require("../models")
 
 const should = chai.should();
 chai.use(chaiHttp);
 
-
-// REFERENCE
-// https://github.com/Thinkful-Ed/blog-app-mongoose-challenge-solution/blob/feature/tests/test/test-blog-posts.js
-
-describe('Trips API resource', function() {
-  before(function() {
+describe('Trips API resource', function () {
+  before(function () {
     // Before the tests run, the server is activated. the`runServer`
     // function returns a promise, and that promise is returned by
     // doing `return runServer`. Without returning a promise here,
@@ -25,41 +21,31 @@ describe('Trips API resource', function() {
   // if another test module is added that also has a `before` block
   // and starts the server, it will cause an error because the
   // server would still be running from the previous tests.
-  after(function() {
+  after(function () {
     return closeServer();
   });
 
-
-
-
-
-
   // tests for GET endpoint
-  describe("GET endpoint", function() {
-    it('should list items on GET', function(done) {
+  describe("GET endpoint", function () {
+    it('should list items on GET', function (done) {
       // for Mocha tests, when dealing with asynchronous operations,
       // a Promise object must be returned or else call a `done` callback
       // at the end of the test. The `chai.request(server).get...` call is asynchronous
       // and returns a Promise, so it must be returned.
       chai.request(app)
-      .get('/trips')
-      .then(function(res) {
-        // console.log(res.body)
-        res.should.have.status(200);
-        done()
-      })
+        .get('/trips')
+        .then(function (res) {
+          // console.log(res.body)
+          res.should.have.status(200);
+          done()
+        })
     })
 
-
-
-
-
-
     // tests for POST endpoint
-    describe("POST endpoint", function() {
-      it("should add a new trip", function(done) {
+    describe("POST endpoint", function () {
+      it("should add a new trip", function (done) {
         const newTrip = {
-          airline:"Delta",
+          airline: "Delta",
           confirmationCode: 1234565,
           tripComments: "going to hotel after arrival",
           departure: {
@@ -75,56 +61,56 @@ describe('Trips API resource', function() {
         }
 
         chai.request(app)
-        .post("/trips/new")
-        .send(newTrip)
-        .then(function(res) {
-          res.should.have.status(201);
-          res.should.be.json
-          res.should.be.an('object');
-          res.body.should.include.keys(
-            'airline', 'confirmationCode', 'tripComments', 'departure', 'arrival'
-          );
-          // Mongo should have created id on insertion
-          res.body.id.should.not.be.null;
+          .post("/")
+          .send(newTrip)
+          .then(function (res) {
+            res.should.have.status(201);
+            res.should.be.json
+            res.should.be.an('object');
+            res.body.should.include.keys(
+              'airline', 'confirmationCode', 'tripComments', 'departure', 'arrival'
+            );
+            // Mongo should have created id on insertion
+            res.body.id.should.not.be.null;
 
-          res.body.airline.should.equal(newTrip.airline);
-          res.body.confirmationCode.should.equal(newTrip.confirmationCode);
-          res.body.tripComments.should.equal(newTrip.tripComments);
+            res.body.airline.should.equal(newTrip.airline);
+            res.body.confirmationCode.should.equal(newTrip.confirmationCode);
+            res.body.tripComments.should.equal(newTrip.tripComments);
 
-          res.body.departure.should.equal(newTrip.departure);
-          res.body.departure.should.be.an("object");
-          res.body.departure.should.include.keys(
-            'airport', 'date', 'transportation'
-          )
+            res.body.departure.should.equal(newTrip.departure);
+            res.body.departure.should.be.an("object");
+            res.body.departure.should.include.keys(
+              'airport', 'date', 'transportation'
+            )
 
-          res.body.arrival.should.equal(newTrip.arrival);
-          res.body.arrival.should.be.an("object");
-          res.body.arrival.should.include.keys(
-            'airport', 'date', 'transportation'
-          )
+            res.body.arrival.should.equal(newTrip.arrival);
+            res.body.arrival.should.be.an("object");
+            res.body.arrival.should.include.keys(
+              'airport', 'date', 'transportation'
+            )
 
-          return Trip.findById(res.body.id);
-        })
-        .then(function(trip) {
-          trip.airline.should.equal(newTrip.airline);
-          trip.confirmationCode.should.equal(newTrip.confirmationCode);
-          trip.tripComments.should.equal(newTrip.tripComments);
-          trip.departure.should.equal(newTrip.departure);
-          trip.arrival.should.equal(newTrip.arrival);
-        });
+            return Trip.findById(res.body.id);
+          })
+          .then(function (trip) {
+            trip.airline.should.equal(newTrip.airline);
+            trip.confirmationCode.should.equal(newTrip.confirmationCode);
+            trip.tripComments.should.equal(newTrip.tripComments);
+            trip.departure.should.equal(newTrip.departure);
+            trip.arrival.should.equal(newTrip.arrival);
+          });
         done();
       })
     })
   })
 
   //test for PUT endpoint
-  describe('PUT endpoint', function() {
+  describe('PUT endpoint', function () {
     // strategy:
     //  1. Get an existing restaurant from db
     //  2. Make a PUT request to update that restaurant
     //  3. Prove restaurant returned by request contains data we sent
     //  4. Prove restaurant in db is correctly updated
-    it('should update fields you send over', function(done) {
+    it('should update fields you send over', function (done) {
       const updateData = {
         airline: 'Delta',
         arrival: {
@@ -134,58 +120,58 @@ describe('Trips API resource', function() {
       };
 
       Trip
-      .findOne()
-      .then(function(trip) {
-        updateData.id = trip.id;
+        .findOne()
+        .then(function (trip) {
+          updateData.id = trip.id;
 
-        // make request then inspect it to make sure it reflects
-        // data we sent
-        chai.request(app)
-        .put(`/trips/${trip.id}`)
-        .send(updateData);
-      })
-      .then(function(res) {
-        res.should.have.status(204);
+          // make request then inspect it to make sure it reflects
+          // data we sent
+          chai.request(app)
+            .put(`/trips/${trip.id}`)
+            .send(updateData);
+        })
+        .then(function (res) {
+          res.should.have.status(204);
 
-        Trip.findById(updateData.id);
-      })
-      .then(function(trip) {
-        trip.airline.should.equal(updateData.airline);
-        trip.arrival[airport].should.equal(updateData.arrival[airport]);
-        trip.arrival[transportation].should.equal(updateData.arrival[transportation]);
-      });
+          Trip.findById(updateData.id);
+        })
+        .then(function (trip) {
+          trip.airline.should.equal(updateData.airline);
+          trip.arrival[airport].should.equal(updateData.arrival[airport]);
+          trip.arrival[transportation].should.equal(updateData.arrival[transportation]);
+        });
       done();
     });
   });
 
   //test for DELETE endpoint
-  describe('DELETE endpoint', function() {
+  describe('DELETE endpoint', function () {
     // strategy:
     //  1. get a restaurant
     //  2. make a DELETE request for that restaurant's id
     //  3. assert that response has right status code
     //  4. prove that restaurant with the id doesn't exist in db anymore
-    it('delete a trip by id', function(done) {
+    it('delete a trip by id', function (done) {
 
       let trip;
 
       Trip
-      .findOne()
-      .then(function(_trip) {
-        trip = _trip;
-        chai.request(app).delete(`/trips/delete/${trip.id}`);
-      })
-      .then(function(res) {
-        res.should.have.status(204);
-        Trip.findById(trip.id);
-      })
-      .then(function(_restaurant) {
-        // when a variable's value is null, chaining `should`
-        // doesn't work. so `_restaurant.should.be.null` would raise
-        // an error. `should.be.null(_restaurant)` is how
-        // assertions can be made about a null value.
-        should.not.exist(_trip);
-      });
+        .findOne()
+        .then(function (_trip) {
+          trip = _trip;
+          chai.request(app).delete(`/trips/delete/${trip.id}`);
+        })
+        .then(function (res) {
+          res.should.have.status(204);
+          Trip.findById(trip.id);
+        })
+        .then(function (_restaurant) {
+          // when a variable's value is null, chaining `should`
+          // doesn't work. so `_restaurant.should.be.null` would raise
+          // an error. `should.be.null(_restaurant)` is how
+          // assertions can be made about a null value.
+          should.not.exist(_trip);
+        });
       done()
     });
   });
