8e6843d0121d622de8e4b3b1d564cfa111eb77c1
Nawar Yossef <nawaryossef2@gmail.com>
2018-01-04 00:08:27 -0500

    delete commented out code

--- public/js/trips.js
+++ public/js/trips.js
@@ -1,17 +1,6 @@
 "use strict";
 console.log(334);
 
-
-// $(() => {
-//   $.ajax({
-//     url: "http://localhost:8081/trips",
-//     type: 'GET',   
-//     success: function(data){
-//       console.log(data)
-//     }
-//   });
-// })
-
 class Trips  {
 
   init() {
