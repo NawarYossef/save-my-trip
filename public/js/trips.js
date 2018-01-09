f591f26e1dabaf7b6a5289a8b7b5e214dd5cb053
Nawar Yossef <nawaryossef2@gmail.com>
2018-01-08 21:30:59 -0500

    add function for slide down effect

--- public/js/trips.js
+++ public/js/trips.js
@@ -6,6 +6,8 @@ class Trips  {
   init() {
     this.getTripEntries();
     this.toggleHamburger();
+    this.slideDownAndShowDetails();
+    this.changeArrowDirection();
   }
 
   getTripEntries() {
@@ -27,6 +29,37 @@ class Trips  {
       }
     })
   }
+
+  changeArrowDirection() {
+    $(".trip-header").click(function(){
+      if ($(this).hasClass("slide-down")) {
+        $(".arrow-wrapper i").removeClass("fa-caret-down");
+        $(".arrow-wrapper i").addClass("fa-caret-up");
+      } else {
+        $(".arrow-wrapper i").removeClass("fa-caret-up");
+        $(".arrow-wrapper i").addClass("fa-caret-down");
+      }
+    });
+  }
+
+  slideDownAndShowDetails() {
+    $(".trip-header").click(function(){
+      if ($(this).hasClass("slide-down")) {
+        $(this).removeClass("slide-down");
+        $(this).addClass("slide-up");
+        $(".arrival-wrapper").slideUp({duration: 450});
+        $(".departure-wrapper").slideUp({duration: 450});  
+      } else {
+        $(this).removeClass("slide-up");
+        $(this).addClass("slide-down");
+        $(".arrival-wrapper").slideDown({duration: 450});
+        $(".departure-wrapper").slideDown({duration: 450});
+        // this.changeArrowDirection();
+      }
+    })
+  }
+
+  
 }
 
 const app = new Trips();
