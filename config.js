35195aea7bb1f54d6994628706be8025d24e2473
Nawar Yossef <nawaryossef2@gmail.com>
2018-01-20 18:13:43 -0500

    add passport Auth strategies

--- config.js
+++ config.js
@@ -1,11 +1,15 @@
 const PORT = process.env.PORT || 8081;
 const DATABASE_NAME = 'save-my-trip';
 const DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || `mongodb://localhost/${DATABASE_NAME}`;
- const TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || `mongodb://localhost/test-${DATABASE_NAME}`
+const TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || `mongodb://localhost/test-${DATABASE_NAME}`
+const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
 
 module.exports = {
   PORT,
   DATABASE_NAME,
   DATABASE_URL,
-  TEST_DATABASE_URL
+  TEST_DATABASE_URL,
+  JWT_SECRET,
+  JWT_EXPIRY
 }
