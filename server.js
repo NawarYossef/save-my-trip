2a8623dbbbfcabcba09da127a1673f16f94272f6
Nawar Yossef <nawaryossef2@gmail.com>
2018-01-20 20:52:15 -0500

    add options to access control allow methods

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(morgan("common"));
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
   next();
 });
 
