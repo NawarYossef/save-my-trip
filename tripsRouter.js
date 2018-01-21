f28bf101a6533f01f586dd1e5cc0b27a7dd27996
Nawar Yossef <nawaryossef2@gmail.com>
2018-01-20 19:06:41 -0500

    add GET endpoint for sending emails

--- tripsRouter.js
+++ tripsRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
-const sgMail = require('@sendgrid/mail');
+// const sgMail = require('@sendgrid/mail');
 
 const {Trip} = require('./models');
 // ============== GET endpoint ==============
